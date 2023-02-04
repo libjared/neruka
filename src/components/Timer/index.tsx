c5e953bb877ea3d427122b1d791b6610756518d8
libjared <libjared@users.noreply.github.com>
2023-02-04 07:44:18 +0000

    WIP: Flesh out overall design

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,21 +1,58 @@
 import { differenceInHours, intervalToDuration } from 'date-fns';
+import { useState } from 'react';
 import DurationInput from '../DurationInput';
 import './Timer.css';
 
+// There are several states for a timer's UX:
+// Rest - The clock shows a duration.
+// Editing - The clock shows a duration in the middle of being updated.
+// Countdown - The clock shows time until a specific point in the future. It must periodically refresh.
+// Overdue - The clock shows time since the alarm hit. It must periodically refresh.
+//
+// All of these states need the logic to display digits in a particular manner. The underlying state that determines
+// WHAT digits to show changes type: rest & editing use a duration, countdown & overdue use a time.
+
+// Timer
+// - holds state, and converts between types.
+// - if alarmTime == null
+// -   child is DurationDisplay
+// -   if currentDuration is not null, we're "paused". send currentDuration.
+// -   otherwise, we're "stopped". send originalDuration.
+// - otherwise
+// -   child is TimeDisplay
+
+// DurationDisplay(duration, onRequestDurationChange)
+// - when not editing, displays the duration without questioning.
+// - when editing, convert the given duration to a state of digits.
+//   - on finished editing, call onRequestDurationChange. if user hit the enter key, callback to make it a quick-start.
+
+// CountdownDisplay(time)
+// - show a countdown (or overdue, depending on the remaining time). useEffect setInterval/clearInterval to refresh it regularly.
+
+// Digit(value, isLit)/Label(label, isLit)
+// - renders one digit or label character.
+
 function Timer() {
-  // const [ alarmTime, setAlarmTime ] = useState<Date | null>(null);
-  // const [ requestedDuration, setRequestedDuration ] = useState<Duration>({ minutes: 15 });
-  // const timeDisplayText = getTimeDisplayText(alarmTime);
-  const onClickSet = () => {
+  const [ originalDuration, setOriginalDuration ] = useState<Duration>({ minutes: 15 });
+  const [ currentDuration, setCurrentDuration ] = useState<Duration | null>(originalDuration);
+  const [ alarmTime, setAlarmTime ] = useState<Date | null>(null);
+
+  const startTimer = () => {
+  };
+  const onRequestDuration = () => {
+  };
+  const onQuickStart = () => {
   };
 
   return (
     <div className="Timer">
       <div className="Timer-display">
-        <DurationInput />
+        <DurationInput
+          onStart={onStartByInput}
+        />
       </div>
       <nav className="Timer-controls">
-        <input type="button" value="Set" onClick={onClickSet} />
+        <input type="button" value="Set" onClick={() => { startTimer(); }} />
         <input type="button" value="Clear" />
       </nav>
     </div>
