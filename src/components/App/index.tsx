dd9cd0b6686b7d72313b2994a4dbeb959eef7cde
libjared <libjared@users.noreply.github.com>
2023-03-16 06:00:31 +0000

    Implement ClockTabs

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Timer from "../Timer";
+import ClockTabs from "../ClockTabs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Neruka - timeboxing timer</header>
       <main className="App-main">
-        <Timer />
+        <ClockTabs />
       </main>
     </div>
   );
--- src/components/ClockTabs/index.tsx
+++ src/components/ClockTabs/index.tsx
@@ -1,5 +1,18 @@
+import Timer from "../Timer";
+import "./ClockTabs.css";
+
 function ClockTabs() {
-  return null;
+  return (
+    <div className="ClockTabs">
+      <nav className="ClockTabs-nav">
+        <ul>
+          <li>Timer</li>
+          <li>Alarm</li>
+        </ul>
+      </nav>
+      <Timer />
+    </div>
+  );
 }
 
 export default ClockTabs;
