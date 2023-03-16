3d4a3685a85f8127565b00766fcbb35f05d25ba5
libjared <libjared@users.noreply.github.com>
2023-03-16 06:03:05 +0000

    Test ClockTabs snapshot

--- src/components/ClockTabs/ClockTabs.test.tsx
+++ src/components/ClockTabs/ClockTabs.test.tsx
@@ -1,5 +1,7 @@
+import { render } from "@testing-library/react";
 import ClockTabs from ".";
 
-it("works", () => {
-  expect(ClockTabs).toBeTruthy();
+it("renders", () => {
+  const { asFragment } = render(<ClockTabs />);
+  expect(asFragment()).toMatchSnapshot();
 });
